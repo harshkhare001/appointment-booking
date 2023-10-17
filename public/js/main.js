af91c3bb8141e783ad175b775356ccba64778364
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-10-17 21:29:54 +0530

    Create main.js

---
+++ public/js/main.js
@@ -0,0 +1,55 @@
+var form = document.getElementById("form");
+
+var list = document.getElementById("list");
+
+async function renderList() {
+  try {
+    const users = await axios.get("http://localhost:3000/users");
+
+    users.data.forEach((elem) => {
+      var li = document.createElement("li");
+      var deleteBtn = document.createElement("button");
+      var editBtn = document.createElement("button");
+
+      li.className = "items";
+      deleteBtn.className = "delete btn btn-dark";
+      editBtn.className = "edit btn btn-info";
+      editBtn.id = elem.id;
+      deleteBtn.id = elem.id;
+      deleteBtn.appendChild(document.createTextNode("Delete"));
+      editBtn.appendChild(document.createTextNode("Edit"));
+      let span1 = document.createElement("span");
+      span1.textContent = `${elem.name}  `;
+      let span2 = document.createElement("span");
+      span2.textContent = `${elem.email}  `;
+      let span3 = document.createElement("span");
+      span3.textContent = `${elem.phone} `;
+      li.appendChild(span1);
+      li.appendChild(span2);
+      li.appendChild(span3);
+
+      li.appendChild(deleteBtn);
+      li.appendChild(editBtn);
+      list.appendChild(li);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+window.addEventListener("DOMContentLoaded", () => {
+  renderList();
+});
+
+async function deleteUser(e) {
+ 
+  if (e.target.classList.contains("delete")) {
+    var li = e.target.parentElement;
+    let id = e.target.id;
+    await axios.get(`http://localhost:3000/deleteUser/${id}`);
+    list.removeChild(li);
+  }
+}
+
+list.addEventListener("click", (e) => {
+  deleteUser(e);
+});
